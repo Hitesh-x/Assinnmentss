d99b334aeda60bfca035b3df9fde71877d00505c
Hitesh_ S <hiteshsingal13@gmail.com>
2025-06-27 22:17:01 +0530

    Day-8

---
+++ Day-8.py
@@ -0,0 +1,62 @@
+
+# ASSIGNMENT
+
+# Question 1
+
+import numpy as np
+
+random_array = np.random.rand(3, 3)
+print("\n1.a) Random Array:\n", random_array)
+
+empty_array = np.empty((4, 2))
+print("\n1.b) Empty Array:\n", empty_array)
+
+full_array = np.full((4, 2), 7)
+print("\n1.c) Full Array:\n", full_array)
+
+zeros_array = np.zeros((3, 5))
+print("\n1.d) Zeros Array:\n", zeros_array)
+
+ones_array = np.ones((4, 3, 2))
+print("\n1.e) Ones Array:\n", ones_array)
+
+#  ==================================================>>
+
+# Question 2
+
+# import numpy as np
+#
+# matrix2to10= np.arange(2, 11).reshape(3, 3)
+# print(" 3x3 2D Matrix with values ranging from 2 to 10:\n", matrix2to10)
+
+# =======================================================>>
+
+# Question 3
+
+# import numpy as np
+#
+# null_vector = np.zeros(10)
+# null_vector[5] = 11
+# print("Null Vector sixth value = 11:\n", null_vector)
+
+# ========================================================>>
+
+# Question 4
+
+# import numpy as np
+#
+# original_arr= np.array([10, 20, 30, 40, 50])
+# reversed_arr = original_arr[::-1]
+# print("Reversed Array:\n", reversed_arr)
+
+# ============================================================>>
+
+# Question 5
+
+# import numpy as np
+#
+# original_arr = np.array([1, 2, 3, 4])
+# float_arr = original_arr.astype(float)
+# print("\nNumpy program  converted to float type:\n", float_arr)
+
+# ======================== FINISH  =============================
